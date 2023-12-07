c87273a9fe74e80a8d6e55bbd3e29fcd1bdce8a3
sureshnanjan <sureshnanjan@outlook.com>
2023-12-07 12:13:47 +0530

    Initial Project skeleton

---
+++ CognizantSeleniumTraining/MyClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognizantSeleniumTraining
+{
+    class MyClass :  ICalculate
+    {
+        // Members
+        int noofParticpants;
+
+        public int Calculate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowMembers() {
+            Console.WriteLine($"{noofParticpants} number of people here");
+        }
+    }
+    // Inheriatnce
+    class mySpecialClass : MyClass {
+        string typeoofclass;
+
+        public void specialShow() {
+            Console.WriteLine("This is specials");
+        }
+
+    }
+
+
+    class SubSubClass : MyClass, ICalculate { 
+        public int Calculate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface ICalculate
+    {
+        int Calculate();
+    }
+
+    interface IPrint{
+        void print();
+}
+
+}
---
+++ CognizantSeleniumTraining/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
+
+namespace CognizantSeleniumTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfBrowser = "chrome";
+            IWebDriver broswer;
+            switch (typeOfBrowser)
+            {
+                case "chrome":
+                    broswer = new ChromeDriver();
+                    break;
+                case "firefox":
+                    broswer = new FirefoxDriver();
+                    break;
+                default:
+                    broswer = new EdgeDriver();
+                    break;
+            }
+        
+            // Navigate to https://the-internet.herokuapp.com/
+            INavigation objectReturned = broswer.Navigate();
+            objectReturned.GoToUrl("https://the-internet.herokuapp.com/");
+            // close the browser
+            // Take a Screen Shot
+            ITakesScreenshot mycamera = broswer as ITakesScreenshot;
+            mycamera.GetScreenshot();
+            IJavaScriptExecutor myscrriptengine = broswer as IJavaScriptExecutor;
+            myscrriptengine.ExecuteScript("alert('This Message')");
+            broswer.Quit();
+            
+
+
+
+            
+            
+            
+
+        }
+    }
+}
