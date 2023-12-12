635c4c1eea119baaff1500d77cb54149c1d49a08
Himanshu Jain <880767@cognizant.com>
2023-12-12 12:54:29 +0530

    Interface Updated for Broken Images and Sorted Data Tables

---
+++ HerokuAppOperations/ISortedDataTables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    public interface ISortedDataTables
+    {
+        void editRow();
+        void deleteRow();
+        void sortTableByEmail();
+        int getRowCount();
+    }
+}
