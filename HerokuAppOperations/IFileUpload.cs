2c51bee157371844fa78fb398282d0a36d18c1fa
Faiz Mohammed Siddiqui <faizmohdsid@gmail.com>
2023-12-13 16:29:15 +0000

    Commit for Interface files for File Upload and Inputs

---
+++ HerokuAppOperations/IFileUpload.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    public interface IFileUpload
+    {
+        /// <summary>
+        /// For getting the heading of the page
+        /// </summary>
+        /// <returns></returns>
+        string getHeading();
+        /// <summary>
+        /// For getting the paragraph description of the page
+        /// </summary>
+        /// <returns></returns>
+        string getDescription();
+        /// <summary>
+        /// For getting status message after uploading the file
+        /// </summary>
+        /// <returns></returns>
+        string getStatusMessage();
+        /// <summary>
+        /// For uploading the file through chose file (file upload control)
+        /// </summary>
+        /// <param name="pathToFile"></param>
+        void uploadThroughChooseFile(string pathToFile);
+        /// <summary>
+        /// For uploading the file through drag and drop
+        /// </summary>
+        /// <param name="pathToFile"></param>
+        void uploadThroughDragDrop(string pathToFile);
+        /// <summary>
+        /// for closing the page
+        /// </summary>
+        void exitApplication();
+    }
+}
---
+++ HerokuAppOperations/IInputs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    public interface IInputs
+    {
+        /// <summary>
+        /// For getting the heading of the page
+        /// </summary>
+        /// <returns></returns>
+        string getHeading();
+        /// <summary>
+        /// For getting the paragraph description of input field
+        /// </summary>
+        /// <returns></returns>
+        string getDescriptionOfInputField();
+        /// <summary>
+        /// For entering the input text into the element
+        /// </summary>
+        /// <returns></returns>
+        void setInput(string inputText);
+        /// <summary>
+        /// for closing the page
+        /// </summary>
+        void exitApplication();
+    }
+}
