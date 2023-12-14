542338037e1a7251257f9321d81acf7f05a098a8
Namita <Namita.Tripathi@gmail.com>
2023-12-14 10:01:03 +0530

    checkbox status

--- HerokuAppOperations/ICheckboxCheckUncheck.cs
+++ HerokuAppOperations/ICheckboxCheckUncheck.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace HerokuAppOperations
 {
+    //This interface is created for Heroko operations to perform validations checks on check box.
     public interface ICheckboxCheckUncheck
     {
+        //This method is to check the first check box status
         bool isBox1Checked();
+        //This method is to check the second check box status
         bool isBox2Checked();        
     }
 }
