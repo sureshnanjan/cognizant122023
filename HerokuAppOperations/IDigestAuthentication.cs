08fbd7674421d01473bbe4b6df86e158a4be42c0
subbum2023 <subbum2008@gmail.com>
2023-12-14 03:51:50 +0000

    test

---
+++ HerokuAppOperations/IDigestAuthentication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    /// <summary>
+    /// This interface will be used to validate User Credentials
+    /// </summary>
+    public interface IDigestAuthentication
+    {
+        /// <summary>
+        /// Validate User Credentials
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        bool ValidateUser(string username, string password);
+
+    }
+}
---
+++ HerokuAppOperations/IFileDownload.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    /// <summary>
+    /// This interface will be used to download file
+    /// </summary>
+    public interface IFileDownload
+    {
+        /// <summary>
+        /// File Download
+        /// </summary>
+        /// <param name="strFileName"></param>
+        /// <returns></returns>
+        bool DownloadFile(string strFileName);
+    }
+}
