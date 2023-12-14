c3849b08f5a0febc497e94aab66e4167ea047e31
Administrator <Administrator@9C55BA92F87A50C>
2023-12-14 10:45:56 +0530

    Commited for DOMChallenging and Slow Resources

---
+++ HerokuAppOperations/IDOMChallengingOperation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+   public interface IDOMChallengingOperation
+    {
+        //Get Sub Heading present in screen
+        string getSubHeading();
+
+        //Get description present in the screen
+        string getDescription();
+
+        //Get Button Text on click
+        void getButtondescription();
+
+        //Get Answer on button click
+        void getAnswer();
+
+        //Get total buttons count
+        void getButtonCount();
+
+        //Get all table elements 
+        string getTableElements();
+
+        //Get all table Row count
+        string getTableRowCount();
+
+        //Perform edit of table element
+        void EditRow(int rowId);
+
+        //Perform Delete of table element
+        void HyperlinkDelete(int rowId);
+
+    }
+}
---
+++ HerokuAppOperations/ISlowResources.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+   public interface ISlowResources
+    {
+        //Get Sub Heading present in screen
+        string getSubHeading();
+
+        //Get Description present in screen
+        string getDescription();
+
+        //Get Load Time
+        int getLoadTime();
+
+    }
+}
