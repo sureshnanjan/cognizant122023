86fe1c9c0c7174e5017754bb8327238a60d60788
balaskarthik <balaskarthik26@gmail.com>
2023-12-14 07:17:00 +0000

    Update the Document

---
+++ HerokuAppOperations/IABTesting.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    /// <summary>
+    /// This interface is intended for ABTesing sub page on HerokuApp Site
+    /// </summary>
+    interface IABTesting
+    {
+        /// <summary>
+        /// This will be used to get the heading of the sub page named as ABTesting. 
+        /// here when user click and comes to ABTesting page, he will see the heading as "A/B Testing" only
+        /// </summary>
+        /// <returns>The return value is a string as the heading of the sub page is alpha and Special symbol </returns>
+        string getHeading();
+
+       /// <summary>
+       /// This will be used to get Contentlength of the Paragraph 
+       /// </summary>
+       /// <returns>This return value is an integer</returns>
+        int getContentLength();
+
+        /// <summary>
+        /// This will be used to return content of the example Paragraph
+        /// </summary>
+        /// <returns>This return value is the string </returns>
+        /// Expected result  (e.g. a user action such as a click-through)
+        string returncontent();
+
+        /// <summary>
+        /// This will be used exit the Application 
+        /// </summary>
+        void exitApplication();
+    }
+}
---
+++ HerokuAppOperations/IWYSIWYGEditor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    /// <summary>
+    /// This interface is intended for WYSIWYGEditor sub page on HerokuApp Site
+    /// </summary>
+    interface IWYSIWYGEditor
+    {
+
+        /// <summary>
+        /// This will be used to get the heading of the sub page named as WYSIWYGEditor. 
+        /// here when user click and comes to WYSIWYGEditor page, he will see the heading as "An iFrame containing the TinyMCE WYSIWYG Editor" only
+        /// </summary>
+        /// <returns>The return value is a string as the heading of the sub page is alpha </returns>
+        string getHeading();
+
+        /// <summary>
+        /// This will be used to get Contentlength of the Paragraph 
+        /// </summary>
+        /// <returns>This return value is an integer</returns>
+        int getContentLength();
+
+        /// <summary>
+        /// This will be used to get the count of the Menubar
+        /// </summary>
+        /// <returns>This return value is an integer</returns>
+        int getMenuBarCount();
+
+        /// <summary>
+        /// This will be used create the new document 
+        /// </summary>
+        void createDocument();
+
+        /// <summary>
+        /// This will be used to check the string empty or not
+        /// </summary>
+        /// <returns>This return value is string</returns>
+        string getcheckEmptycontent();
+
+
+        /// <summary>
+        /// This will be used getcontent details
+        /// </summary>
+        /// <returns>This return value is string</returns>
+        string getContent();
+
+        /// <summary>
+        /// This will be used undo document 
+        /// </summary>
+        /// <returns></returns>
+        string undoDocument();
+
+        /// <summary>
+        /// This will be used redo document 
+        /// </summary>
+        /// <returns></returns>
+        string redoDocument();
+
+
+        /// <summary>
+        /// This will be used to get the Statusbar data
+        /// </summary>
+        /// <returns>This return value is a string</returns>
+        /// expected result - (Powered by Tiny)
+        string getStatusBarDetails();
+
+        /// <summary>
+        /// This will be used to exit Application
+        /// </summary>
+        void exitApplication();
+    }
+}
