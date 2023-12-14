9c09f13ee0c4a7e8548098f8c17c8b2fa9572f69
balaskarthik <balaskarthik26@gmail.com>
2023-12-14 07:17:00 +0000

    Update the Document

--- HerokuAppOperations/IWYSIWYGEditor.cs
+++ HerokuAppOperations/IWYSIWYGEditor.cs
@@ -6,33 +6,72 @@ using System.Threading.Tasks;
 
 namespace HerokuAppOperations
 {
+    /// <summary>
+    /// This interface is intended for WYSIWYGEditor sub page on HerokuApp Site
+    /// </summary>
     interface IWYSIWYGEditor
     {
-        // Get the title
+
+        /// <summary>
+        /// This will be used to get the heading of the sub page named as WYSIWYGEditor. 
+        /// here when user click and comes to WYSIWYGEditor page, he will see the heading as "An iFrame containing the TinyMCE WYSIWYG Editor" only
+        /// </summary>
+        /// <returns>The return value is a string as the heading of the sub page is alpha </returns>
         string getHeading();
 
-        // check the contentlength
-        string getContentLength();
+        /// <summary>
+        /// This will be used to get Contentlength of the Paragraph 
+        /// </summary>
+        /// <returns>This return value is an integer</returns>
+        int getContentLength();
 
-        // Get the count of Menubar
-        string getMenuBarCount();
+        /// <summary>
+        /// This will be used to get the count of the Menubar
+        /// </summary>
+        /// <returns>This return value is an integer</returns>
+        int getMenuBarCount();
 
-        // Open the Filemenu
-        void fileElement();
+        /// <summary>
+        /// This will be used create the new document 
+        /// </summary>
+        void createDocument();
 
-        // Open the EditMenu
-        void editElement();
+        /// <summary>
+        /// This will be used to check the string empty or not
+        /// </summary>
+        /// <returns>This return value is string</returns>
+        string getcheckEmptycontent();
 
-        // Open the Viewmenu
-        void viewElement();
 
-        // Open the Formatmenu
-        void formatElement();
+        /// <summary>
+        /// This will be used getcontent details
+        /// </summary>
+        /// <returns>This return value is string</returns>
+        string getContent();
 
-        // Get the Statusbar
+        /// <summary>
+        /// This will be used undo document 
+        /// </summary>
+        /// <returns></returns>
+        string undoDocument();
+
+        /// <summary>
+        /// This will be used redo document 
+        /// </summary>
+        /// <returns></returns>
+        string redoDocument();
+
+
+        /// <summary>
+        /// This will be used to get the Statusbar data
+        /// </summary>
+        /// <returns>This return value is a string</returns>
+        /// expected result - (Powered by Tiny)
         string getStatusBarDetails();
 
-        // Exit the Application
+        /// <summary>
+        /// This will be used to exit Application
+        /// </summary>
         void exitApplication();
     }
 }
