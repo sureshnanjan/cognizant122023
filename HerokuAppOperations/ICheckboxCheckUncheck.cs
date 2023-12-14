62395398af0ed4bd69f949702b5e942356bbc98e
Namita <Namita.Tripathi@gmail.com>
2023-12-14 11:36:53 +0530

    changes

--- HerokuAppOperations/ICheckboxCheckUncheck.cs
+++ HerokuAppOperations/ICheckboxCheckUncheck.cs
@@ -7,31 +7,31 @@ using System.Threading.Tasks;
 namespace HerokuAppOperations
 {
     ///<summery> 
-    ///This interface is created for Heroko operations to perform validations checks on check box.
+    ///This interface is created for Heroko operations to perform validation checks on check box.
     ///</summery>
 
     public interface ICheckboxCheckUncheck
     {
         ///<summery>
-        ///This method is to check the first check box checked
+        ///This method is to check the first check box checked or not
         ///</summery>
-        ///<returns></returns>
+        ///<returns>returns the check box status</returns>
         bool isBox1Checked();
 
         ///<summery>
-        ///This method is to check the second check box checked
+        ///This method is to check the second check box checked or not
         ///</summery>
-        ///<returns></returns>
+        ///<returns>returns the check box status</returns>
         bool isBox2Checked();
 
         ///<summery>
-        ///This method is to check the fisrt check box unchecked
+        ///This method is to check the fisrt check box unchecked or not
         ///</summery>
-        ///<returns></returns>
+        ///<returns>returns the check box status</returns>
         bool isBox1Unchecked();
 
         ///<summery>
-        ///This method is to check the second check box unchecked
+        ///This method is to check the second check box unchecked or not
         ///</summery>
         ///<returns>returns the check box status</returns>
         bool isBox2Unchecked();
