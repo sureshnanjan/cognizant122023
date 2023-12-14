7f6ea84f752ba1287a1fc3276ee0b8e6ea23dfbf
salmanthowfeeq <thowfeeqsalman1992@gmail.com>
2023-12-14 18:57:38 +0530

    added example floating menu and infinite scroll

---
+++ HerokuAppOperations/IFloatingMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+  public  interface IFloatingMenu
+    {
+        /// <summary>
+        /// Get the heading to check text is equal or not
+        /// </summary>
+        /// <returns></returns>
+        string getheading();
+
+        string getForkmeDetails();
+        /// <summary>
+        /// 4 floating menu's are available , to get this menu count
+        /// </summary>
+        /// <returns></returns>
+        int getFloatMenuCount();
+        /// <summary>
+        /// scroll down and check floating menu is still visible
+        /// </summary>
+        /// <returns></returns>
+        string Scrolldown();
+        /// <summary>
+        /// scroll up and check floating menu is still visible
+        /// </summary>
+        /// <returns></returns>
+        string ScrollUp();
+    }
+}
---
+++ HerokuAppOperations/IInfiniteScroll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+   public interface IInfiniteScroll
+    {
+        /// <summary>
+        /// Get the heading to check equal or not
+        /// </summary>
+        /// <returns></returns>
+        string getheading();
+        /// <summary>
+        /// Get the description to compare with the paragraph equal
+        /// </summary>
+        /// <returns></returns>
+        string getDescription();
+        /// <summary>
+        /// when we scroll down , that scroll bar size gets decrease by this we can find the postion of the scroll bar
+        /// </summary>
+        /// <returns></returns>
+        string ScrollBarPosition();
+    }
+}
