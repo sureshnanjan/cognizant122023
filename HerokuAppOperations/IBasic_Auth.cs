e94d104789089c5cf6c4df1341733cab98fcec45
Arnab999 <arnabganguli70@gmail.com>
2023-12-14 21:41:45 +0530

    interface added

---
+++ HerokuAppOperations/IBasic_Auth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    interface IBasic_Auth
+    {
+        bool SuccessfullOnClickSign_in(string uname,string pass);
+        bool OnClickCancel();
+        bool InvalidOnClickSign_in(string uname,string pass);
+        string BasicAuth_getHeading();
+        string BasicAuth_getDescription();
+        string getForkmeDetails();
+    }
+}
---
+++ HerokuAppOperations/IStatus_Codes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    interface IStatus_Codes
+    {
+        string getHeading();
+        string getDescription();
+        string getForkmeDetails();
+        int getStatusCodesCount();
+        string OnClickCompleteListOfStatusCodes();
+        void goToStatusCodes(string statusCode);
+    }
+}
