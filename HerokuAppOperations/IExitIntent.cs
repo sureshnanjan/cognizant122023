5df8205b4bdd7528c1051759e76db11f36c1cead
sammanthamm <mani4205@gmail.com>
2023-12-14 19:22:11 +0530

    Interface changes has been done

--- HerokuAppOperations/IExitIntent.cs
+++ HerokuAppOperations/IExitIntent.cs
@@ -8,13 +8,37 @@ namespace HerokuAppOperations
 {
     public interface IExitIntent
     {
+
+        /// <summary>
+        /// Grabbing the Main heading text
+        /// </summary>
+        /// <returns></returns>
+        /// 
         string ExitIntendPageHeading();
 
+
+        /// <summary>
+        /// Grabbing the sub heading text
+        /// </summary>
+        /// <returns></returns>
         string ExitIntendsubHeading();
 
+
+        /// <summary>
+        /// verify popupwindow display after moved to exit intent page 
+        /// </summary>
         void VerifyPopupwindowprompting();
 
+
+        /// <summary>
+        ///Grabbing the text from the pop window
+        /// </summary>
+        /// <returns></returns>
         string gettingwindowtext();
+
+        /// <summary>
+        /// click the clode button verify the pop should be disaapeared
+        /// </summary>
 
         void closewindow();
 
---
+++ HerokuAppOperations/IJavaScriptAlertOperation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+   public  interface IJavaScriptAlertOperation
+    {
+        /// <summary>
+        /// Grabing the text from heading 
+        /// </summary>
+        /// <returns></returns>
+        string gettingpageheading();
+
+        /// <summary>
+        /// click the JSAlert button and check pop-up windows is opening 
+        /// close the pop up window verify windows is closing 
+        /// </summary>
+        void verifyJsAlertfunction();
+        /// <summary>
+        /// click the Jsconfirm button and check pop-up windows is opening 
+        /// close the pop up window verify windows is closing
+        /// </summary>
+        void verifyJsConfirmfunction();
+
+        /// <summary>
+        /// click the Jsprompt button and check pop-up windows is opening 
+        /// close the pop up window verify windows is closing
+        /// </summary>
+        
+        void verifyJspromptfunction();
+
+        /// <summary>
+        /// open each js alert button function pop-up window grabbing the text from window 
+        /// </summary>
+        /// <returns></returns>
+        string gettextfromjswindow();
+
+        /// <summary>
+        /// open each js alert button function pop-up window which has cancel function
+        /// check cance function working 
+        /// </summary>
+        void cancelJSwindow();
+
+        /// <summary>
+        /// open js alert function 
+        /// send the text to the text field 
+        /// </summary>
+        void entertextintojswindow();
+
+
+        /// <summary>
+        /// Click the each js alert function grabbing the text which is displayed under the resut section 
+        /// </summary>
+        /// <returns></returns>
+        string getresult();
+
+
+
+
+    }
+}
