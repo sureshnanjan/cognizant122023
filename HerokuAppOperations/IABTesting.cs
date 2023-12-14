c7bfc4d378614f93c2eef68d7619c1fb463d47e2
balaskarthik <balaskarthik26@gmail.com>
2023-12-14 04:24:49 +0000

    Added the Interface of the ABTesting and WYSIWYEditor

---
+++ HerokuAppOperations/IABTesting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    interface IABTesting
+    {
+        // Get the Title
+        string getHeading();
+
+        // Check the Contentlength
+        string getContentLength();
+
+        // Get the useraction
+        void userAction();
+
+        // Exit the application
+        void exitApplication();
+    }
+}
---
+++ HerokuAppOperations/IWYSIWYGEditor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    interface IWYSIWYGEditor
+    {
+        // Get the title
+        string getHeading();
+
+        // check the contentlength
+        string getContentLength();
+
+        // Get the count of Menubar
+        string getMenuBarCount();
+
+        // Open the Filemenu
+        void fileElement();
+
+        // Open the EditMenu
+        void editElement();
+
+        // Open the Viewmenu
+        void viewElement();
+
+        // Open the Formatmenu
+        void formatElement();
+
+        // Get the Statusbar
+        string getStatusBarDetails();
+
+        // Exit the Application
+        void exitApplication();
+    }
+}
