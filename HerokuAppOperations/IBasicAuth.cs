1d5f4bf592838a1602e10895681c7ca6519bb489
Arnab999 <arnabganguli70@gmail.com>
2023-12-15 12:20:41 +0530

    Interface creation

---
+++ HerokuAppOperations/IBasicAuth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    public interface IBasicAuth
+    {
+        // To get Dialog Box
+        bool getDialogBox();
+
+        // On Successfull Login
+        bool onClickSuccessfullLogin(string uname, string pass);
+
+        // If user gives empty user credentials
+        bool onclickEmptyCredentialsLogin(string uname, string pass);
+
+        // If user clicks cancel
+        bool onClickCancel();
+
+        // To get the heading
+        string getHeading();
+
+        // To get the description
+        string getDescription();
+    }
+}
