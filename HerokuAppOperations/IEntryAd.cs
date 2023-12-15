67e01196b9300a79cd33f77c47b2a278369b1a0e
tejaswwini-coder <tejaswinisalunkheprogrammer@gmail.com>
2023-12-15 10:35:28 +0530

    interface creation

---
+++ HerokuAppOperations/IEntryAd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    public interface IEntryAd
+    {
+        /// <summary>
+        /// Get username from home page, if available
+        /// </summary>
+        /// <returns></returns>
+        string getUsername();
+
+        /// <summary>
+        /// Open pop-up window from main page
+        /// </summary>
+        /// <param name="username"></param>
+        void openPopUp(string username);
+
+        /// <summary>
+        /// Close Pop-up window on 'close' button click
+        /// </summary>
+        void closePopUp();
+
+    }
+}
---
+++ HerokuAppOperations/IJavaScriptOnLoadEventError.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    public interface IJavaScriptOnloadEventError
+    {
+        /// <summary>
+        /// If there is any JavaScript error, display the message
+        /// </summary>
+        /// <returns></returns>
+        string getErrorMessage();
+    }
+}
