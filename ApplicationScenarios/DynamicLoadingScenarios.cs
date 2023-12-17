45a385fce4511bdeb308c1ec593798beab0b8be4
Ankit_Sahu <ww.ankitsahu.com@gmail.com>
2023-12-17 20:37:23 +0530

    Implementation of Dynamic Loading Scenarios

---
+++ ApplicationScenarios/DynamicLoadingScenarios.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using HerokuAppOperations;
+using System;
+using HerokuPages;
+
+namespace ApplicationScenarios
+{
+    [TestFixture]
+    class DynamicLoadingScenarios
+    {
+        [Test]
+        [Author("Ankit Sahu")]
+        [Category("Dynamic Loading")]
+        public void DynamicLoadingHeading()
+        {
+            //Arrange
+            IDynamicLoading page = new DynamicLoadingImpl();
+            //Act
+            String  dynamicPageHeading =page.getHeading();
+            // Assert
+            Assert.That(dynamicPageHeading, Is.EqualTo("Dynamically Loaded Page Elements"));
+        }
+
+        [Test]
+        [Author("Ankit Sahu")]
+        [Category("Dynamic Loading")]
+        public void Example1Textverify()
+        {
+            //Arrange
+            IDynamicLoading page = new DynamicLoadingImpl();
+            //Act
+            String dynamicPageHeading = page.getExample1Heading();
+            // Assert
+            Assert.That(dynamicPageHeading, Is.EqualTo("Example 1: Element on page that is hidden"));
+        }
+
+        [Test]
+        [Author("Ankit Sahu")]
+        [Category("Dynamic Loading")]
+        public void Example2Textverify()
+        {
+            //Arrange
+            IDynamicLoading page = new DynamicLoadingImpl();
+            //Act
+            String dynamicPageHeading = page.getExample2Heading();
+            // Assert
+            Assert.That(dynamicPageHeading, Is.EqualTo("Example 2: Element rendered after the fact"));
+        }
+        [Test]
+        [Author("Ankit Sahu")]
+        [Category("Dynamic Loading")]
+        public void Example1PageSubHeadingVerify()
+        {
+            //Arrange
+            IDynamicLoading page = new DynamicLoadingImpl();
+            //Act
+            String dynamicPageHeading = page.getExample1PageSubHeading();
+            // Assert
+            Assert.That(dynamicPageHeading, Is.EqualTo("Example 1: Element on page that is hidden"));
+        }
+
+        [Test]
+        [Author("Ankit Sahu")]
+        [Category("Dynamic Loading")]
+        public void Example2PageSubHeadingVerify()
+        {
+            //Arrange
+            IDynamicLoading page = new DynamicLoadingImpl();
+            //Act
+            String dynamicPageHeading = page.getExample2PageSubHeading();
+            // Assert
+            Assert.That(dynamicPageHeading, Is.EqualTo("Example 2: Element rendered after the fact"));
+        }
+
+        [Test]
+        [Author("Ankit Sahu")]
+        [Category("Dynamic Loading")]
+        public void Example1DyanamicLoadingVerify()
+        {
+            //Arrange
+            IDynamicLoading page = new DynamicLoadingImpl();
+            //Act
+            String starttext=page.DoExample1();
+            // Assert
+            Assert.That(starttext, Is.EqualTo("Hello World!"));
+        }
+    }
+}
---
+++ HerokuPages/DynamicLoadingImpl.cs
@@ -0,0 +1,88 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HerokuAppOperations;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace HerokuPages
+{
+    public class DynamicLoadingImpl:IDynamicLoading
+    {
+        private IWebDriver _browser;
+        private string _url = "https://the-internet.herokuapp.com/dynamic_loading";
+       
+        public DynamicLoadingImpl()
+        {
+            _browser = new ChromeDriver();
+            _browser.Navigate().GoToUrl(_url);
+        }
+
+        public String DoExample1()
+        {
+            _browser.FindElement(By.XPath("//*[@id='content']/div/a[1]")).Click();
+            _browser.FindElement(By.XPath("//*[@id='start']/button")).Click();
+            return _browser.FindElement(By.XPath("//*[@id='finish']/h4")).Text; 
+        }
+
+        public String  DoExample2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getExample1Heading()
+        {
+          return  _browser.FindElement(By.XPath("//*[@id='content']/div/a[1]")).Text;
+
+        }
+          
+        public string getExample1PageSubHeading()
+        {
+            _browser.FindElement(By.XPath("//*[@id='content']/div/a[1]")).Click();
+           return _browser.FindElement(By.XPath("//*[@id='content']/div/h4")).Text;
+        }
+
+        public string getExample2Heading()
+        {
+            return _browser.FindElement(By.XPath("//*[@id='content']/div/a[2]")).Text;
+
+        }
+
+        public string getExample2PageSubHeading()
+        {
+            _browser.FindElement(By.XPath("//*[@id='content']/div/a[2]")).Click();
+            Thread.Sleep(30000);
+            return _browser.FindElement(By.XPath("//*[@id='content']/div/h4")).Text;
+        }
+
+        public string getForkmeDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getHeading()
+        {
+             return _browser.FindElement(By.XPath("//*[@id='content']/div/h3")).Text;
+
+        }
+
+        public string getParagrapghText()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void navigateExample1()
+        {
+            _browser.FindElement(By.XPath("//*[@id='content']/div/a[1]")).Click();
+        }
+
+        public void navigateExample2()
+        {
+            _browser.FindElement(By.XPath("//*[@id='content']/div/a[2]")).Click();
+        }
+    }
+}
